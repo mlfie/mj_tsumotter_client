d6cd7fd2c69d8874be1a6a11a18b41f546e4c926
ginok <miragino@gmail.com>
2012-03-16 12:13:48 +0900

    Agari用の単体テストクラス。現在はDummyAgariだが、そのうちAgariへ変える。

---
+++ MjTsumotterUnitTest/AgariUnitTest.m
@@ -0,0 +1,34 @@
+//
+//  AgariUnitTest.m
+//  MjTsumotter
+//
+//  Created by 健 荻野 on 12/03/16.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "AgariUnitTest.h"
+#import "DummyAgari.h"
+#import "SBJson.h"
+
+@implementation AgariUnitTest
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+- (void)testToJSON
+{
+    DummyAgari *agari = [[DummyAgari alloc] init];
+    agari.total_point = 1000;
+    agari.total_fu_num = 20;
+    NSString *json = [agari toJSON];
+    NSLog(@"toJSON = %@", json);
+}
+
+@end
